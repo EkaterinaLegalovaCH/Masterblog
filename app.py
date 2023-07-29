359d1bf41cd5a4001cc2576a07062da50d536d42
EkaterinaLegalovaCH <legalovaev83@gmail.com>
2023-07-29 02:16:26 +0200

    Updated app.py

--- app.py
+++ app.py
@@ -1,42 +1,13 @@
-from flask import Flask, render_template, request
-from datetime import datetime
+from flask import Flask
+
 
 app = Flask(__name__)
-users = {
-    'Alice': {'age': 25, 'country': 'USA'},
-    'Bob': {'age': 30, 'country': 'UK'},
-    'Charlie': {'age': 35, 'country': 'Australia'}
-}
 
 
 @app.route('/')
-def index():
-    user = request.args.get('name')
-    if not user:
-        user = "Anonymous"
-    return render_template('index.html', title='Home', name=user, users=users, time=datetime.now())
-
-
-@app.route('/greet/<name>')
-def greet(name):
-    return render_template('index.html', title='Home', name=name)
-
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return f"post id is {post_id}."
-
-
-@app.route('/all-users')
-def all_users():
-    return render_template('all-users.html', users=users)
-
-
-@app.route('/form')
-def form():
-    return render_template('form.html', title='Form')
+def hello_world():
+    return 'Hello World!'
 
 
 if __name__ == "__main__":
-    # Launch the Flask dev server
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
